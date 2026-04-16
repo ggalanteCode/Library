85544a20e4a8fe564d99accfa2cdb47547184b2e
ggalanteCode <ggalante3@gmail.com>
2026-04-16 20:02:55 +0200

    User: add EnumType.STRING for role field

--- src/main/java/com/generation153/library/entity/User.java
+++ src/main/java/com/generation153/library/entity/User.java
@@ -2,6 +2,8 @@ package com.generation153.library.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,6 +22,7 @@ public class User {
 	@Column (nullable = false, unique = true, length =128)
 	private String email;
 	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
 	private EnumRole role;
 	@Column(nullable = false)
 	private boolean blocked;
