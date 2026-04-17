4df832a8234e112d2b6956a6491964dd6adf0e15
ggalanteCode <ggalante3@gmail.com>
2026-04-17 19:03:41 +0200

    User: add lombok, and jpa annotations to User

--- src/main/java/com/generation153/library/entity/User.java
+++ src/main/java/com/generation153/library/entity/User.java
@@ -8,12 +8,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.java.Log;
 
+@Log
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)	//auto_increment
 	private Integer id;
 	@Column(name="first_name", nullable = false, length =128)
 	private String firstName;
